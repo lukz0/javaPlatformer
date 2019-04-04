646d1865b73c0a6b289a7fcc30c131e180a6d4b9
Lukasz Filip Mrozik <lukafm18@uia.no>
2019-04-04 19:12:38 +0200

    fullscreen is now again off by default

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
 public class Main {
     // 1 for Vsync, 0 for no Vsync
     static final int VSYNC = 1;
-    static final boolean FULLSCREEN = true;
+    static final boolean FULLSCREEN = false;
 
     public static void main(String[] args) {
         new Controller();
