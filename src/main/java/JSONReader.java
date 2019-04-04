b4ab0049cb3ed1f817ce3582eea90257940694f7
Lukasz Filip Mrozik <lukafm18@uia.no>
2019-04-04 19:11:42 +0200

    Fixed JSONReader error i made by merging

--- src/main/java/JSONReader.java
+++ src/main/java/JSONReader.java
@@ -1,4 +1,3 @@
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -14,27 +13,15 @@ public class JSONReader {
     private static final JSONParser parser = new JSONParser();
 
     //Object here is actually a JSON object, the parser to make them tiles is yet to be implemented given that we don't have a Tile class
-    //TODO: when a Tile class/Chunk class is created, make a converter from JSONObject to that
-    static ArrayList<Object> ReadFile(String path) {
-        ArrayList<Object> result = new ArrayList<>();
+    static JSONObject ReadFile(String path) {
+        JSONObject obj = null;
+        try (FileReader reader = new FileReader(path)) {
+            obj = (JSONObject) parser.parse(reader);
 
-        static JSONObject ReadFile(String path) {
-            JSONObject obj = null;
-            try (FileReader reader = new FileReader(path)) {
-
-                JSONArray parsed = (JSONArray) parser.parse(reader);
-                //parsed.forEach(obj -> result.add(obj));
-                //parsed.forEach(result::add);
-                obj = (JSONObject) parser.parse(reader);
-
-                //i think this does the same as the commented out version, if it doesn't work inverse it, else just use the forEach over.
-                Collections.addAll(result,parsed);
-
-            } catch (IOException | ParseException e) {
-                e.printStackTrace();
-            }
-            return result;
-            return obj;
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
         }
-
+        return obj;
     }
+
+}
