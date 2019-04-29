0f3ca61401b359157a14e97cf708bf5b228ddfbb
Sebastian Bekkvik Aas <sebasa18@student.uia.no>
2019-04-29 20:26:29 +0200

    Added brick blocks

---
+++ src/main/java/Game/Brick.java
@@ -0,0 +1,66 @@
+package Game;
+
+import Level.Block.AbstractBlock;
+import Level.Chunk;
+import Level.Entity;
+import Level.Level;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Brick extends Entity {
+    Vector3f translation = Vector3f.EMPTY, velocity = Vector3f.EMPTY;
+    private static final int STATE_IDLE = 1;
+    int currentState;
+    Level level;
+
+    public Brick(View view, HashMap<String, Async<Texture>> textures, long timestamp, double xPos, double yPos, int chunkIndex, Level level) {
+        this.width = 1;
+        this.height = 1;
+        this.level = level;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.xVelocity = 0;
+        this.yVelocity = 0;
+        this.chunkIndex = chunkIndex;
+        if (!textures.containsKey("brick.png")) {
+            textures.put("brick.png", view.loadTexture("resources/images/brick.png"));
+        }
+
+        Async<Renderer.Drawable> idleSprite = view.getNewTexturedRectangle(0, 1, 1, 0, -0.5f, textures.get("primeGoomb_fwd.png"),
+                this.translation, this.velocity, timestamp);
+
+        HashMap<Integer, Async<Renderer.Drawable>> states = new HashMap<>();
+        states.put(Game.Brick.STATE_IDLE, idleSprite);
+
+        this.drawableID = view.createPosUpdateableGroup(this.translation, this.velocity, states, timestamp);
+        this.currentState = Brick.STATE_IDLE;
+        view.setActiveState(this.drawableID, this.currentState);
+    }
+
+    @Override
+    public void doMove(ArrayList<Chunk> chunks, Gameloop gameloop, long tickStart) {}
+
+    @Override
+    public boolean collisionEntBlc(ArrayList<ArrayList<AbstractBlock>> target) {
+        return false;
+    }
+
+    @Override
+    public void updatePos() {}
+
+    @Override
+    public void pause(View view) {
+
+    }
+
+    @Override
+    public void unPause(View view) {
+
+    }
+
+    @Override
+    public boolean collisionEntEnt(Entity target) {
+        return false;
+    }
+}
