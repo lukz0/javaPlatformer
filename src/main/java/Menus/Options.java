8449976ee9d21917da498e6e51b89635a7f17535
Lukasz Filip Mrozik <lukafm18@uia.no>
2019-04-30 13:51:25 +0200

    added vsync to options

--- src/main/java/Menus/Options.java
+++ src/main/java/Menus/Options.java
@@ -13,13 +13,38 @@ public class Options extends Menu {
     private final Menu parent;
     private final Gameloop gameloop;
     private final HashMap<String, Async<Texture>> textures;
+    final Options that;
+
+    static class SliderStates {
+        boolean vsync, fullscreen;
+        int width, height;
+        SliderStates() {
+            Renderer.Options options = JSONReader.ReadOptions();
+            this.fullscreen = options.fullscreen;
+            this.height = options.height;
+            this.width = options.width;
+            this.vsync = options.vsync;
+        }
+        boolean equalsInitial() {
+            return this.vsync == Main.INITIAL_RENDERING_OPTIONS.vsync &&
+                    this.fullscreen == Main.INITIAL_RENDERING_OPTIONS.fullscreen &&
+                    this.width == Main.INITIAL_RENDERING_OPTIONS.width &&
+                    this.height == Main.INITIAL_RENDERING_OPTIONS.height;
+        }
+    }
+    SliderStates sliderStates = new SliderStates();
 
     Options(Menu parent, Gameloop gameloop, View view) {
+        that = this;
         this.parent = parent;
         this.view = view;
         this.gameloop = gameloop;
         this.textures = new HashMap<String, Async<Texture>>() {
             {
+                this.put("vsync_on_active", view.loadTexture("resources/GUI/Sliders/vsync_on_active.png"));
+                this.put("vsync_on_inactive", view.loadTexture("resources/GUI/Sliders/vsync_on_inactive.png"));
+                this.put("vsync_off_active", view.loadTexture("resources/GUI/Sliders/vsync_off_active.png"));
+                this.put("vsync_off_inactive", view.loadTexture("resources/GUI/Sliders/vsync_off_inactive.png"));
                 this.put("discard_active", view.loadTexture("resources/GUI/Buttons/discard_active.png"));
                 this.put("discard_inactive", view.loadTexture("resources/GUI/Buttons/discard_inactive.png"));
                 this.put("apply_active", view.loadTexture("resources/GUI/Buttons/apply_active.png"));
@@ -30,14 +55,14 @@ public class Options extends Menu {
         try {
             this.menu = new GU_Menu(this.view, new ArrayList<GU_Menu.MenuItemInfo>() {
                 {
-                    this.add(new GU_Menu.SimpleButtonInfo(new ExitHandler(), textures.get("discard_inactive"), textures.get("discard_active")));
-                    this.add(new GU_Menu.SimpleButtonInfo(new ExitHandler(), textures.get("apply_inactive"), textures.get("apply_active")));
-                    this.add(new GU_Menu.SliderInfo(new EmptyEnterHandler(), new StateChangePrinter(), new ArrayList<GU_Button.Textures>() {
+                    this.add(new GU_Menu.SliderInfo(new EmptyEnterHandler(), new VsyncStateChanger(sliderStates), new ArrayList<GU_Button.Textures>() {
                         {
-                            this.add(new GU_Button.Textures(textures.get("discard_inactive"), textures.get("discard_active")));
-                            this.add(new GU_Button.Textures(textures.get("apply_inactive"), textures.get("apply_active")));
+                            this.add(new GU_Button.Textures(textures.get("vsync_on_inactive"), textures.get("vsync_on_active")));
+                            this.add(new GU_Button.Textures(textures.get("vsync_off_inactive"), textures.get("vsync_off_active")));
                         }
-                    }));
+                    }, (that.sliderStates.vsync) ? 0 : 1));
+                    this.add(new GU_Menu.SimpleButtonInfo(new ExitHandler(), textures.get("discard_inactive"), textures.get("discard_active")));
+                    this.add(new GU_Menu.SimpleButtonInfo(new ApplyHandler(), textures.get("apply_inactive"), textures.get("apply_active")));
                 }
             }, -0.9f);
         } catch (Exception e) {
@@ -67,8 +92,19 @@ public class Options extends Menu {
     private class ExitHandler extends GU_Button.EnterEventHandler {
         public void enter() {
             gameloop.setCurrentMenu(parent);
-            pause();
             parent.unPause();
+            pause();
+        }
+    }
+    private class ApplyHandler extends GU_Button.EnterEventHandler {
+        public void enter() {
+            gameloop.setCurrentMenu(parent);
+            parent.unPause();
+            pause();
+            if (!sliderStates.equalsInitial()) {
+                JSONReader.WriteOptions(new Renderer.Options(sliderStates.width, sliderStates.height, sliderStates.fullscreen, sliderStates.vsync));
+                System.out.println("Changes to config writte, please restart the game");
+            }
         }
     }
     private static class EmptyEnterHandler extends GU_Button.EnterEventHandler {
@@ -77,6 +113,15 @@ public class Options extends Menu {
     private static class StateChangePrinter extends GU_Slider.SlideEventHandler {
         public void state(int state) {
             System.out.println("[OPTIONS] Changed state to ".concat(Integer.toString(state)));
+        }
+    }
+    private static class VsyncStateChanger extends GU_Slider.SlideEventHandler {
+        private final SliderStates states;
+        VsyncStateChanger(SliderStates states) {
+            this.states = states;
+        }
+        public void state(int state) {
+            states.vsync = (state == 0);
         }
     }
 }
