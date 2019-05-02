df47979678c8265e2e6843db4486ffac804d42e0
Sebastian Bekkvik Aas <sebasa18@student.uia.no>
2019-05-02 13:49:19 +0200

    Made bricks change form when 'broken'

--- src/main/java/Game/Brick.java
+++ src/main/java/Game/Brick.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 public class Brick extends Entity {
     Vector3f translation = Vector3f.EMPTY, velocity = Vector3f.EMPTY;
     private static final int STATE_IDLE = 1;
+    private static final int STATE_BROKEN = 2;
     int currentState;
     Level level;
 
@@ -26,12 +27,18 @@ public class Brick extends Entity {
         if (!textures.containsKey("brick.png")) {
             textures.put("brick.png", view.loadTexture("resources/images/brick.png"));
         }
+        if (!textures.containsKey("ground.png")) {
+            textures.put("ground.png", view.loadTexture("resources/images/ground.png"));
+        }
 
         Async<Renderer.Drawable> idleSprite = view.getNewTexturedRectangle(0, 1, 1, 0, -0.5f, textures.get("brick.png"),
+                this.translation, this.velocity, timestamp);
+        Async<Renderer.Drawable> brokenSprite = view.getNewTexturedRectangle(0, 1, 1, 0, -0.5f, textures.get("ground.png"),
                 this.translation, this.velocity, timestamp);
 
         HashMap<Integer, Async<Renderer.Drawable>> states = new HashMap<>();
         states.put(Game.Brick.STATE_IDLE, idleSprite);
+        states.put(Brick.STATE_BROKEN, brokenSprite);
 
         this.drawableID = view.createPosUpdateableGroup(this.translation, this.velocity, states, timestamp);
         this.currentState = Brick.STATE_IDLE;
@@ -39,7 +46,12 @@ public class Brick extends Entity {
     }
 
     @Override
-    public void doMove(ArrayList<Chunk> chunks, Gameloop gameloop, long tickStart) {}
+    public void doMove(ArrayList<Chunk> chunks, Gameloop gameloop, long tickStart) {
+        if(!this.interactable) {
+            this.currentState = Brick.STATE_BROKEN;
+            gameloop.view.setActiveState(this.drawableID, this.currentState);
+        }
+    }
 
     @Override
     public boolean collisionEntBlc(ArrayList<ArrayList<AbstractBlock>> target) {
