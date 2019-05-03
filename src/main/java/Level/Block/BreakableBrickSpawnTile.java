9ac612a95a587d521649ffa70b3806aeeffc90d1
Lukasz Filip Mrozik <lukafm18@uia.no>
2019-05-03 14:48:33 +0200

    added breakable brick to block list

--- src/main/java/Level/Block/BreakableBrickSpawnTile.java
+++ src/main/java/Level/Block/BreakableBrickSpawnTile.java
@@ -1,9 +1,6 @@
 package Level.Block;
 
-import Game.Async;
-import Game.Brick;
-import Game.Texture;
-import Game.View;
+import Game.*;
 import Level.Level;
 
 import java.util.HashMap;
@@ -13,7 +10,7 @@ public class BreakableBrickSpawnTile  extends NonStaticAbstractBlock {
     public void init(Level level, View view, HashMap<String, Async<Texture>> textures, long timestamp, int chunkIndex, int xPos, int yPos) {
         if (!alreadyUsed) {
             alreadyUsed = true;
-            level.addEntityToChunk(chunkIndex, new Brick(view, textures, timestamp, xPos, yPos, chunkIndex, level), view);
+            level.addEntityToChunk(chunkIndex, new BreakableBrick(view, textures, timestamp, xPos, yPos, chunkIndex, level), view);
         }
     }
 }
