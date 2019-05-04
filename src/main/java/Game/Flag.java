e3449e2499a36750a656be732ce3e8b93c1d3ad1
Sebastian Bekkvik Aas <sebasa18@student.uia.no>
2019-05-04 15:45:04 +0200

    Added end-of-level flags

---
+++ src/main/java/Game/Flag.java
@@ -0,0 +1,47 @@
+package Game;
+
+import Level.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Flag extends Entity {
+    Vector3f translation = Vector3f.EMPTY, velocity = Vector3f.EMPTY;
+    private static final int STATE_IDLE = 1;
+    int currentState;
+    Level level;
+
+    public Flag(View view, HashMap<String, Async<Texture>> textures, long timestamp, double xPos, double yPos, int chunkIndex, Level level) {
+        this.width = 1;
+        this.height = 1;
+        this.level = level;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.xVelocity = 0;
+        this.yVelocity = 0;
+        this.chunkIndex = chunkIndex;
+        if (!textures.containsKey("flag.png")) {
+            textures.put("flag.png", view.loadTexture("resources/images/flag.png"));
+        }
+
+        Async<Renderer.Drawable> idleSprite = view.getNewTexturedRectangle(0, 1, 1, 0, -0.5f, textures.get("flag.png"),
+                this.translation, this.velocity, timestamp);
+
+        HashMap<Integer, Async<Renderer.Drawable>> states = new HashMap<>();
+        states.put(this.STATE_IDLE, idleSprite);
+
+        this.drawableID = view.createPosUpdateableGroup(this.translation, this.velocity, states, timestamp);
+        this.currentState = this.STATE_IDLE;
+        view.setActiveState(this.drawableID, this.currentState);
+    }
+
+    @Override
+    public void doMove(ArrayList<Chunk> chunks, Gameloop gameloop, long tickStart) {
+
+    }
+
+    @Override
+    public void updatePos() {
+
+    }
+}
